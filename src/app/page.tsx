f38445907c21556905029b5e6218690c10d15dcc
gerhynes <gkhynes@gmail.com>
2026-04-13 20:34:22 +0100

    feat: better-auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="my-6 px-4 max-w-md mx-auto">
       <h1 className="text-center space-y-6">Welcome to Our App</h1>
-      <Link href="/auth/login">Sign In / Sign Up</Link>
+      <Button asChild size="lg">
+        <Link href="/auth/login">Sign In / Sign Up</Link>
+      </Button>
     </div>
   );
 }
