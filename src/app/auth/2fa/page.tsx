a07d23beb74122db2e13863ddaf26f9438e9e950
gerhynes <gkhynes@gmail.com>
2026-05-19 23:03:45 +0100

    feat: Two-factor auth

---
+++ src/app/auth/2fa/page.tsx
@@ -0,0 +1,39 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { auth } from "@/lib/auth/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { BackupCodeTab } from "./_components/backup-code-tab";
+import { TotpForm } from "./_components/totp-form";
+
+export default async function TwoFactorPage() {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (session != null) return redirect("/");
+
+  return (
+    <div className="my-6 px-4">
+      <Card className="w-full max-w-md mx-auto">
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl font-bold">
+            Two-Factor Authentication
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Tabs defaultValue="totp">
+            <TabsList className="grid w-full grid-cols-2 mb-8">
+              <TabsTrigger value="totp">Authenticator</TabsTrigger>
+              <TabsTrigger value="backup">Backup Codes</TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="totp">
+              <TotpForm />
+            </TabsContent>
+            <TabsContent value="backup">
+              <BackupCodeTab />
+            </TabsContent>
+          </Tabs>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
